863a77cf9f51c45f124fe2a215aab0cc541c9d41
vctrjrvs <vctrjrvs@gmail.com>
2025-01-17 00:07:48 -0600

    fixed some of the tests

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,15 @@ test('renders Hello Mello link', () => {
   const linkElement = screen.getByText(/Hello Mello/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders Welcome to Mello header', () => {
+  render(<App />);
+  const headerElement = screen.getByText(/Welcome to Mello/i);
+  expect(headerElement).toBeInTheDocument();
+});
+
+test('renders footer text', () => {
+  render(<App />);
+  const footerElement = screen.getByText(/©.*Mello. All Rights Reserved./i);
+  expect(footerElement).toBeInTheDocument();
+});
